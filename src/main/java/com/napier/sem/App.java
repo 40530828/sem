b6b552cc92fb160e8bd4b40bf2db3dd0a489a255
malkybar <40530828@live.napier.ac.uk>
2023-02-22 15:41:56 +0000

    stuff

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -68,8 +68,8 @@ public class App
             }
         }
     }
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         // Create new Application
         App a = new App();
 
